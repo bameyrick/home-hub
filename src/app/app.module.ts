46d0037a3721f8053cd98ee20145353566f9b32b
Ben Meyrick <bameyrick@gmail.com>
2023-01-10 23:52:17 +0000

    Fix sockets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ import { ROOT_REDUCERS, RoutingModule } from './routing';
     EffectsModule.forRoot([]),
     RoutingModule,
     SocketIoModule.forRoot({
-      url: `http://localhost:${environment.port}`,
+      url: `${location.origin}:${environment.port}`,
     }),
   ],
   providers: [],
