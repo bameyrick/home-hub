672b17b7ea4cd0991290bb1d5e320a727cddffef
Ben Meyrick <bameyrick@gmail.com>
2023-06-08 15:56:49 +0100

    Fix temperatue colours

--- src/app/weather/tempeature/weather-temperature.directive.ts
+++ src/app/weather/tempeature/weather-temperature.directive.ts
@@ -12,20 +12,24 @@ export class WeatherTemperatureDirective implements OnChanges {
   constructor(private readonly elementRef: ElementRef) {}
 
   public ngOnChanges(): void {
+    this.setColours();
+  }
+
+  private setColours(): void {
     if (isNumber(this.temperature)) {
       let backgroundColour: string = Colour.darkBlue;
       let textColour: Colour = Colour.black;
 
       if (this.temperature > 40) {
         backgroundColour = Colour.red;
-      } else if (this.temperature > 30) {
-        backgroundColour = mixColours(Colour.red, Colour.orange, this.getPercentage(30, 40, this.temperature));
-      } else if (this.temperature > 20) {
-        backgroundColour = mixColours(Colour.orange, Colour.yellow, this.getPercentage(20, 30, this.temperature));
-      } else if (this.temperature > 10) {
-        backgroundColour = mixColours(Colour.yellow, Colour.lightBlue, this.getPercentage(10, 20, this.temperature));
+      } else if (this.temperature > 25) {
+        backgroundColour = mixColours(Colour.red, Colour.orange, this.getPercentage(25, 40, this.temperature));
+      } else if (this.temperature > 15) {
+        backgroundColour = mixColours(Colour.orange, Colour.yellow, this.getPercentage(15, 25, this.temperature));
+      } else if (this.temperature > 5) {
+        backgroundColour = mixColours(Colour.yellow, Colour.lightBlue, this.getPercentage(5, 15, this.temperature));
       } else if (this.temperature > 0) {
-        backgroundColour = mixColours(Colour.lightBlue, Colour.darkBlue, this.getPercentage(0, 10, this.temperature));
+        backgroundColour = mixColours(Colour.lightBlue, Colour.darkBlue, this.getPercentage(0, 5, this.temperature));
       }
 
       if (this.temperature <= 10) {
@@ -40,6 +44,6 @@ export class WeatherTemperatureDirective implements OnChanges {
   private getPercentage(from: number, to: number, value: number): number {
     const difference = Math.abs(from - to);
 
-    return Math.abs((value - difference) / difference);
+    return Math.abs((value - from) / difference);
   }
 }
